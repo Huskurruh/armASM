a2b54b7a5acc76fdf3a742373dc9cb0c2fb69116
Adrian Rosén <31214867+Huskurruh@users.noreply.github.com>
2018-06-26 21:27:57 +0200

    Add subroutine to print ascii message to stdout and code to test

--- prog32.s
+++ prog32.s
@@ -1,6 +1,33 @@
-		.global _start
+	.global _start
 
 _start:
-	mov	R0, #0	@ All went fine
-	mov 	R7, #1	@ Tell scv to terminate
-	svc 	0	@ Terminate program
+	ldr	R1, =msgText	@ Pointer to text for subroutine
+	mov	R2, #12		@ Length of message to display for subroutine
+	bl	displayAscii	@ Branch and link to subroutine
+	mov	R0, #0		@ Code for normal termination
+	mov	R7, #1		@ Code to terminate program
+	svc	0		@ Terminate program
+
+	.data
+
+msgText:
+	.ascii	"Test string\n"
+
+	.text
+
+	@ Subroutines
+	@ 	displayAscii, display ascii message to stdout
+	@		R1: Pointer to start of ascii message
+	@		R2: Length of message
+	@		LR: Contains return address
+	@		Consumed registers: R0, R7
+
+displayAscii:
+	mov	R0, #1		@ Code to write to stdout
+	mov	R7, #4		@ Code for svc to write string
+	svc	0		@ Command to write string
+	bx	LR		@ Return to calling program
+
+	.end
+
+	
